f7e8defcc93ce65ecb99796c8a66739d8651c813
Michal Raca <mraca@op.pl>
2016-01-25 22:21:06 +0100

    drugi dzien

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,7 +1,22 @@
 class BoardsController < ApplicationController
 
-def index
-	@boards = Board.find(1)
-end
+	def index
+		@boards = Board.all
+	end
 
+	def destroy
+		Board.find(params[:id]).destroy
+		redirect_to boards_url
+	end
+
+	def create
+		@board = Board.new(params[:name => 'Dupa'])
+		if @board.save
+			redirect_to boards_path
+		end
+	end
+
+	def new
+		redirect_to boards_path
+	end
 end
